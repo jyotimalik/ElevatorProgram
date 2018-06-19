23ad7566c82b5309ab019aa7944e7d6cfb97cda3
jyoti <malikjyoti000@gmail.com>
2018-06-19 19:08:44 +0530

    Initial commit

---
+++ Elevator.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorProgram
+{
+    public class Elevator: IElevator
+    {
+        //Defaults and Declarations
+        //building has n floors
+
+        private readonly Dictionary<int,bool> _floorReady= new Dictionary<int, bool>();
+        private int _currentLevel = 1;
+        private readonly int _upperLevel;
+        private readonly int _lowerLevel;
+        private ElevatorStatus _status = ElevatorStatus.STOPPED;
+
+        public Elevator(int lowerLevel=0,int upperLevel = 5)
+        {
+            CalculateFloor(lowerLevel, upperLevel);
+            _upperLevel = upperLevel;
+            _lowerLevel = lowerLevel;
+        }
+
+        private void CalculateFloor(int lowerLevel , int upperLevel )
+        {
+           
+            for (int i = lowerLevel; i <= upperLevel; i++)
+            {
+                _floorReady.Add(i, false);
+            }
+           
+        }
+
+        private void Stop(int floor)
+        {
+            _status = ElevatorStatus.STOPPED;
+            _currentLevel = floor;
+            _floorReady[floor] = false;
+            Console.WriteLine("Stopped at floor {0}", floor);
+        }
+
+        private void Descend(int floor)
+        {
+            for (int i = _currentLevel; i >= 1; i--)
+            {
+                if (_floorReady[i])
+                    Stop(floor);              
+            }
+
+            _status = ElevatorStatus.STOPPED;
+            Console.WriteLine("Waiting..");
+        }
+
+        private void Ascend(int floor)
+        {
+            for (int i = _currentLevel; i <= _upperLevel; i++)
+            {
+                if (_floorReady[i])
+                    Stop(floor);             
+            }
+
+            _status = ElevatorStatus.STOPPED;
+            Console.WriteLine("Waiting..");
+        }
+
+        void StayPut()
+        {
+            Console.WriteLine("That's our current floor");
+        }
+
+        public void FloorPress(int floor)
+        {
+            if (floor > _upperLevel)
+            {
+                Console.WriteLine("We only have {0} floors", _upperLevel);
+                return;
+            }
+
+            if (floor <_lowerLevel)
+            {
+                Console.WriteLine("We only have {0} level", _lowerLevel);
+                return;
+            }
+            _floorReady[floor] = true;
+
+            switch (_status)
+            {
+
+                case ElevatorStatus.DOWN:
+                    Descend(floor);
+                    break;
+
+                case ElevatorStatus.STOPPED:
+                    if (_currentLevel < floor)
+                        Ascend(floor);
+                    else if (_currentLevel == floor)
+                        StayPut();
+                    else
+                        Descend(floor);
+                    break;
+
+                case ElevatorStatus.UP:
+                    Ascend(floor);
+                    break;
+            }
+
+
+        }
+
+        public enum ElevatorStatus
+        {
+            UP,
+            STOPPED,
+            DOWN
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ElevatorProgram
+{
+    class Program
+    {
+        private const string QUIT = "q";
+        static void Main(string[] args)
+        {                      
+            
+            int floor;  IElevator elevator;          
+          
+           elevator = new Elevator();
+          
+            string input = string.Empty;
+
+            while (input != QUIT)
+            {
+                Console.WriteLine("Please press which floor you would like to go to");
+
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out floor))
+                    elevator.FloorPress(floor);
+                else if (input == QUIT)
+                    Console.WriteLine("GoodBye!");
+                else
+                    Console.WriteLine("You have pressed an incorrect floor, Please try again");
+            }
+        }
+    }
+}
